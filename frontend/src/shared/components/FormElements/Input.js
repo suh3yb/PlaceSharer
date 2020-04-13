4c091666d3fc092f358cb9aa83811f09aa8cef7e
Suheyb Coskun <suheyb.c@gmail.com>
2020-04-13 17:07:38 +0200

    add input, newplace components, input validator functions

---
+++ frontend/src/shared/components/FormElements/Input.js
@@ -0,0 +1,86 @@
+import React, { useReducer, useEffect } from 'react';
+
+import { validate } from '../../util/validators';
+import './Input.css';
+
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.value,
+        isValid: validate(action.value, action.validators),
+      };
+    case 'TOUCH':
+      return {
+        ...state,
+        isTouched: true,
+      };
+    default:
+      return state;
+  }
+};
+
+const Input = ({
+  element,
+  id,
+  label,
+  type,
+  placeholder,
+  rows,
+  errorText,
+  validators,
+  onInput,
+}) => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: '',
+    isValid: false,
+    isTouched: false,
+  });
+
+  useEffect(() => {
+    onInput(id, inputState.value, inputState.isValid);
+  }, [id, onInput, inputState.value, inputState.isValid]);
+
+  const changeHandler = event => {
+    dispatch({ type: 'CHANGE', value: event.target.value, validators });
+  };
+
+  const touchHandler = () => {
+    dispatch({ type: 'TOUCH' });
+  };
+
+  const elem =
+    element === 'input' ? (
+      <input
+        id={id}
+        type={type}
+        placeholder={placeholder}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
+    ) : (
+      <textarea
+        id={id}
+        rows={rows || 3}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
+    );
+
+  return (
+    <div
+      className={`form-control ${
+        !inputState.isValid && inputState.isTouched && 'form-control--invalid'
+      }`}
+    >
+      <label htmlFor={id}>{label}</label>
+      {elem}
+      {!inputState.isValid && inputState.isTouched && <p>{errorText}</p>}
+    </div>
+  );
+};
+
+export default Input;
