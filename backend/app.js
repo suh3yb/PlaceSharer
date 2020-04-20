eeac3f81e9777adfa595f8fa0e730049c25f9057
Suheyb Coskun <suheyb.c@gmail.com>
2020-04-20 17:44:54 +0200

    add error handling

--- backend/app.js
+++ backend/app.js
@@ -9,4 +9,13 @@ const app = express();
 
 app.use('/api/places', placesRoutes);
 
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res
+    .status(error.code || 500)
+    .json({ message: error.message || 'An unknown error occurre d.' });
+});
+
 app.listen(5000);
