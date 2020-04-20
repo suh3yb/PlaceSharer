076b5d33513fb65a17f2c42b70216680d5d7c100
Suheyb Coskun <suheyb.c@gmail.com>
2020-04-21 01:38:31 +0200

    add logic to get coordinates using an address from Google

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -3,6 +3,7 @@
 const { v4: uuid } = require('uuid');
 const { validationResult } = require('express-validator');
 
+const getCoordsForAddress = require('../util/location');
 const HttpError = require('../models/http-error');
 
 let DUMMY_PLACES = [
@@ -58,13 +59,22 @@ const getPlacesByUserId = (req, res, next) => {
   res.json({ places });
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError('Invalid inputs passed, please check your data.', 422);
+    return next(
+      new HttpError('Invalid inputs passed, please check your data.', 422)
+    );
   }
 
-  const { title, description, coordinates, address, creator } = req.body;
+  const { title, description, address, creator } = req.body;
+
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(error);
+  }
 
   const createdPlace = {
     id: uuid(),
