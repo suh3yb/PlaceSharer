1e6ae1b373e46712f2fea10379e3451f9a6a8a2d
Suheyb Coskun <suheyb.c@gmail.com>
2020-05-28 15:48:42 +0200

    add and configure multer as middleware to handle image upload in backend, change frontend to send image to backend

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -4,6 +4,7 @@ const express = require('express');
 const { check } = require('express-validator');
 
 const { getUsers, login, signup } = require('../controllers/users-controllers');
+const fileUpload = require('../middleware/file-upload');
 
 const router = express.Router();
 
@@ -11,6 +12,7 @@ router.get('/', getUsers);
 
 router.post(
   '/signup',
+  fileUpload.single('image'),
   [
     check('name').not().isEmpty(),
     check('email').normalizeEmail().isEmail(),
